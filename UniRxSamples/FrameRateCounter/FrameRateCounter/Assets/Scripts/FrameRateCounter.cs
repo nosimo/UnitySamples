b63f2c9f8a1e805db73103ff89c284c964c40d71
nosi <spoken0904@gmail.com>
2017-01-10 00:55:46 +0900

    FrameRate(FPS)を表示するUniRxのサンプルを追加

---
+++ UniRxSamples/FrameRateCounter/FrameRateCounter/Assets/Scripts/FrameRateCounter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+using UnityEngine;
+using UniRx;
+
+public static class FrameRateCounter
+{
+    //!@note
+    // http://qiita.com/toRisouP/items/1d0682e7a35cdb04bc38
+    // ↑の内容ほぼそのままです.
+
+    /// <summary>
+    /// FrameRate計測用のバッファ.
+    /// </summary>
+    const Int32 BufferSize = 5;
+    /// <summary>
+    /// 現在のFPS値.
+    /// 外部からはSubscribeすることで値変更を監視可能.
+    /// </summary>
+    public static IReadOnlyReactiveProperty<float> Current { get; private set; }
+
+    /// <summary>
+    /// コンストラクタ.
+    /// </summary>
+    static FrameRateCounter()
+    {
+        Current = Observable.EveryUpdate()
+            .Select(_ => Time.deltaTime)
+            .Buffer(BufferSize, 1)
+            .Select(
+                (x) =>
+                {
+                    var ave = x.Average();
+                    return (ave > 0.0f) ? (1.0f / ave) : 0.0f;
+                })
+            .ToReadOnlyReactiveProperty();
+    }
+}
---
+++ UniRxSamples/FrameRateCounter/FrameRateCounter/Assets/Scripts/FrameRateTextPresenter.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UniRx;
+
+/// <summary>
+/// FrameRateTextとFrameRateCounter.CurrentをつなぐためのPresenter.
+/// </summary>
+public class FrameRateTextPresenter : MonoBehaviour
+{
+    // Use this for initialization
+    void Start()
+    {
+        // FrameRateCounter.Current→Text.
+        FrameRateCounter.Current
+            .Subscribe(
+                (x) =>
+                {
+                    var text = GetComponent<Text>();
+                    if (text != null)
+                    {
+                        text.text = x.ToString();
+                    }
+                })
+            .AddTo(this);
+    }
+}
